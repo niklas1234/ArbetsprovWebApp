de1a88e64976f58d44a21490f1c94b578c0e8129
Niklas Allard <niklas.allard@enfogroup.com>
2020-02-18 19:05:00 +0100

    Refact

--- ArbetsprovWebApp/Controllers/DockanController.cs
+++ ArbetsprovWebApp/Controllers/DockanController.cs
@@ -1,18 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
+﻿using ArbetsprovWebApp.Services;
+using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
 namespace ArbetsprovWebApp.Controllers
 {
     public class DockanController : Controller
-    {
-        static readonly HttpClient client = new HttpClient();
+    {        
         private string blobDataTemperature1;
         private string blobDataHumidity1;
         private string blobDataRainfall1;
+        private const string blobURI = "https://sigmaiotexercisetest.blob.core.windows.net/iotbackend/dockan";
 
         [HttpGet]
         public async Task<string> DataAsync(string id, string sensorType)
@@ -20,33 +17,20 @@ namespace ArbetsprovWebApp.Controllers
             switch (sensorType)
             {
                 case null: //No sensortype
-                    string blobDataHumidity = await CallBlobAPI("https://sigmaiotexercisetest.blob.core.windows.net/iotbackend/dockan/humidity/" + id + ".csv");
-                    string blobDataTemperature = await CallBlobAPI("https://sigmaiotexercisetest.blob.core.windows.net/iotbackend/dockan/temperature/" + id + ".csv");
-                    string blobDataRainfall = await CallBlobAPI("https://sigmaiotexercisetest.blob.core.windows.net/iotbackend/dockan/rainfall/" + id + ".csv");
+                    string blobDataHumidity = await FetchDataService.CallBlobAPI(blobURI + "/humidity/" + id + ".csv");
+                    string blobDataTemperature = await FetchDataService.CallBlobAPI(blobURI + "/temperature/" + id + ".csv");
+                    string blobDataRainfall = await FetchDataService.CallBlobAPI(blobURI + " /rainfall/ " + id + ".csv");
                     return "Humidity \r\n" + blobDataHumidity + "Rainfall \r\n" + blobDataRainfall + "Temperature \r\n" + blobDataTemperature;                    
 
                 case "temperature":
-                    return blobDataTemperature1 = await CallBlobAPI("https://sigmaiotexercisetest.blob.core.windows.net/iotbackend/dockan/temperature/" + id + ".csv");
+                    return blobDataTemperature1 = await FetchDataService.CallBlobAPI(blobURI + "/temperature/" + id + ".csv");
                 case "humidity":
-                    return blobDataHumidity1 = await CallBlobAPI("https://sigmaiotexercisetest.blob.core.windows.net/iotbackend/dockan/humidity/" + id + ".csv");
+                    return blobDataHumidity1 = await FetchDataService.CallBlobAPI(blobURI + "/humidity/" + id + ".csv");
                 case "rainfall":
-                    return blobDataRainfall1 = await CallBlobAPI("https://sigmaiotexercisetest.blob.core.windows.net/iotbackend/dockan/rainfall/" + id + ".csv");
+                    return blobDataRainfall1 = await FetchDataService.CallBlobAPI(blobURI + "/rainfall/" + id + ".csv");
                 default:
                     return "Sensortype not found.";
             }            
-        }
-
-        static async Task<string> CallBlobAPI(string BlobURI)
-        {
-            try
-            {
-                string responseBody = await client.GetStringAsync(BlobURI);
-                return responseBody;
-            }
-            catch (HttpRequestException e)
-            {
-                return e.ToString();
-            }
         }
     }    
 }
---
+++ ArbetsprovWebApp/Services/FetchDataService.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ArbetsprovWebApp.Services
+{
+    public class FetchDataService
+    {
+        static readonly HttpClient client = new HttpClient();
+        public static async Task<string> CallBlobAPI(string BlobURI)
+        {
+            try
+            {
+                string responseBody = await client.GetStringAsync(BlobURI);
+                return responseBody;
+            }
+            catch (HttpRequestException e)
+            {
+                return e.ToString();
+            }
+        }
+    }
+}
