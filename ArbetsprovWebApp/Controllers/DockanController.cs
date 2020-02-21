771f1122ca5044eda6d12047d24a1743c7e83ded
Niklas Allard <niklas.allard@enfogroup.com>
2020-02-21 09:07:21 +0100

    Response -> json

--- ArbetsprovWebApp/Controllers/DockanController.cs
+++ ArbetsprovWebApp/Controllers/DockanController.cs
@@ -1,12 +1,11 @@
 ﻿using ArbetsprovWebApp.Models;
 using ArbetsprovWebApp.Services;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
-
+using System.Text.Json;
+using Newtonsoft.Json;
 
 namespace ArbetsprovWebApp.Controllers
 {
@@ -28,27 +27,39 @@ namespace ArbetsprovWebApp.Controllers
                     var blobDataTemperature = await FetchDataService.CallBlobAPI(blobURI + "/temperature/" + id + ".csv");
                     var blobDataRainfall = await FetchDataService.CallBlobAPI(blobURI + "/rainfall/" + id + ".csv");
 
+                    var outgoingObjAsList = new List<Temperature>();
+                    var jsonList = new List<Temperature>();
                     var x = new List<Temperature>();
-
                     var listofTemperatures = blobDataTemperature.Split("\r\n").ToList();
+                    var json = System.Text.Json.JsonSerializer.Serialize(listofTemperatures);  //Skapa lista i foreach och kör sen denna raden
                     foreach (var temperature in listofTemperatures)
                     {
+                        if (!string.IsNullOrEmpty(temperature)) 
+                        { 
                         var temperatureData = temperature.Split(";");
-                        var pointinTime = temperatureData[0];
-                        var temper = temperatureData[1];
-                        var temperatureObj = new Temperature();
-                        temperatureObj.PointInTime = pointinTime;
-                        temperatureObj.TemperatureCelsius = temper;
-                        var json = JsonConvert.SerializeObject(temperatureObj);
+                        //var pointinTime = temperatureData[0];
+                        //var temper = temperatureData[1];
+                        Temperature outgoingJson = new Temperature()
+                        {
+                            PointInTime = temperatureData[0],
+                            TemperatureCelsius = temperatureData[1]
+                        };
+                        //var temperatureObj = new Temperature();
+                        //temperatureObj.PointInTime = pointinTime;
+                        //temperatureObj.TemperatureCelsius = temper;
+                        //var json = JsonConvert.SerializeObject(temperatureObj);
+                        //var json = JsonSerializer.Serialize(aList);                        
 
-                        //Newtonsoft.Json.JsonConvert.SerializeObject(new {foo = "bar"})
-
-                        //var obj = JsonConvert.DeserializeObject<Temperature>();
+                        outgoingObjAsList.Add(outgoingJson);
+                        }
                     }
+                    return System.Text.Json.JsonSerializer.Serialize(outgoingObjAsList);
+                //var JSONresult = JsonConvert.SerializeObject(outgoingObjAsList.OrderBy(kaffe => kaffe.StartDate).ThenBy(kaffe => kaffe.LastModifiedDateTime));
+                //var JSONresult = JsonConvert.SerializeObject(outgoingObjAsList);
 
-
-                    return "Humidity \r\n" + blobDataHumidity + "Rainfall \r\n" + blobDataRainfall + "Temperature \r\n" + blobDataTemperature;
-                case "temperature":
+                // return JSONresult;
+                //return "Humidity \r\n" + blobDataHumidity + "Rainfall \r\n" + blobDataRainfall + "Temperature \r\n" + blobDataTemperature;
+                case "temperature":                    
                     return blobDataTemperature1 = await FetchDataService.CallBlobAPI(blobURI + "/temperature/" + id + ".csv");
                 case "humidity":
                     return blobDataHumidity1 = await FetchDataService.CallBlobAPI(blobURI + "/humidity/" + id + ".csv");
